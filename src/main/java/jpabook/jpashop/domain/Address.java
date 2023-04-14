7757adce36ce703f8cc7ef32746291b44aa8cece
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-14 20:22:50 +0900

    FEAT: add entity class 2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,9 +6,17 @@ import javax.persistence.Embeddable;
 
 @Embeddable // 내장타입이라는 표시
 @Getter
-public class Address {
+public class Address { // Address -> 값의 변경이 불가능한 클래스 만들기
 
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    protected Address() {} // JPA 구현 라이브러리가 객체를 생성할 때 리플릭션, 프록시 기술을 사용해야 하기 때문에 기본 생성자 만듬
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders") // 이름 바꿔주기 order -> orders
 @Getter @Setter
@@ -17,18 +19,36 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id") // FK라는 표시
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    // JPQL -> SELECT o FROM order o; -> SQL SELECT * FROM order -> (N + 1)
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // cascade -> persist 전파
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
-    private LocalDateTime orderDate; // 주문시간
+    private LocalDateTime orderDate; // 주문시간, DB -> order_date
 
     private OrderStatus status; // 주문상태 [ORDER, STATUS]
+
+    //==연관관계 편의 메소드==// -> 양방향일 떄 좋다
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this); // this = member
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this); // this = orderItem
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
